b24710b35b7b18285eea88946dc294b5c64e7288
Loren Cipriano <loren25cipriano@gmail.com>
2021-06-20 13:06:41 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Add your Circle class here
+
+// Circle
+// ✓ correctly returns the radius property of a Circle instance using `circle.radius`
+// gets
+//   ✓ the diameter of a circle using the pseudo-property `circle.diameter`
+//   ✓ the circumference of a circle using the pseudo-property `circle.circumference`
+//   ✓ the area of a circle using the pseudo-property `circle.area`
+// sets
+//   ✓ the radius using the pseudo-property `circle.diameter`
+//   ✓ the radius using the pseudo-property `circle.circumference`
+//   ✓ the radius using the pseudo-property `circle.area`
+
+
+
+class Circle {
+    constructor(radius){
+        this.radius = radius;
+    }
+
+    get diameter() {
+        return this.radius * 2
+    }
+
+    get circumference(){
+        return 3.14 * this.diameter
+    }
+
+    get area() {
+        return 3.14 * (this.radius * this.radius)
+    }
+
+    set diameter(diameter) {
+        this.radius = diameter / 2
+    } 
+
+    set circumference(circumference) {
+        this.radius = circumference / (3.14 * 2)
+    }
+
+    set area( area ) {
+        this.radius = Math.sqrt( area / 3.14 )
+    }
+}
+
+
